063f4f74ff16278327444f82fce9d6aeeb6d7b27
Woo Mobil <woomobil@Woo-iMac.local>
2020-03-20 17:17:10 +0300

    libs dosyaları üst düzeye çıkarıldı

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-import * as array from './libs/array';
-import * as date from './libs/date';
-import * as language from './libs/language';
-import * as price from './libs/price';
-import * as string from './libs/string';
-
-var onesignal = null;
+import * as array from './array';
+import * as date from './date';
+import * as language from './language';
+import * as price from './price';
+import * as string from './string';
 
 export default {
     array,
     date,
     language,
-    onesignal: () => {
-        if (onesignal == null) {
-            import * as util from './libs/onesignal';
-            onesignal = util;
-        }
-        return onesignal;
-    },
     price,
     string
 }
