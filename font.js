ea2e317f4aef8fbf566afc5705bb46f7ac415efe
Yusuf Çağrı Oğuz <yco@Yusuf-MacBook-Pro.local>
2020-03-26 17:14:35 +0300

    react native font eklendi

---
+++ font.js
@@ -0,0 +1,26 @@
+/**
+* Verilen metnin ekrana göre fontayarını oranlar
+* @param percent metin parametresidir.
+*/
+import { isIphoneX } from "react-native-iphone-x-helper";
+import { Platform, StatusBar, Dimensions } from "react-native";
+
+const { height, width } = Dimensions.get("window");
+const standardLength = width > height ? width : height;
+
+const deviceHeight = isIphoneX()
+    ? standardLength - 78 // iPhone X style SafeAreaView size in portrait
+    : Platform.OS === "android"
+        ? standardLength - StatusBar.currentHeight
+        : standardLength;
+
+export function RFPercentage(percent) {
+    const heightPercent = (percent * deviceHeight) / 100;
+    return Math.round(heightPercent);
+}
+
+// guideline height for standard 5" device screen is 680
+export function RFValue(fontSize, standardScreenHeight = 680) {
+    const heightPercent = (fontSize * deviceHeight) / standardScreenHeight;
+    return Math.round(heightPercent);
+}
